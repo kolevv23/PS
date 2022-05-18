d14ace267dd473939fdef0ac6545b27a4a9778d4
kolevv23 <vaseto902z@gmail.com>
2022-05-18 21:20:05 +0300

    wrong commit

--- WpfHello1/obj/Debug/net5.0-windows/MainWindow.g.i.cs
+++ WpfHello1/obj/Debug/net5.0-windows/MainWindow.g.i.cs
@@ -133,7 +133,7 @@ namespace WPFHello1 {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/WpfHello1;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/WpfHello1;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
